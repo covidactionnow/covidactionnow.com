657e75f50df7655a27e17e8bc233620459fc90a8
spsooo <spsooo@users.noreply.github.com>
2024-06-07 20:32:18 -0400

    configure map with markers

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # covidactionnow.com
+
+TODO
+
+- document map hosting + how to get tiles
+
+- document map marker update process
+
+- document localization
+
+- use prospectus-L for hero section?
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,23 @@
 import { z, defineCollection } from "astro:content";
 
+const distro = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      name: z.string(),
+      website: z.string().url().optional(),
+      email: z.string().email().optional(),
+      insta: z.string().optional(),
+      address: z.string().optional(),
+      tel: z.string().optional(),
+      latLon: z
+        .tuple([z.number().min(-180).max(180), z.number().min(-90).max(90)])
+        .optional(),
+      description: z.string().optional(),
+    }),
+  ),
+});
+
 const locale = defineCollection({
   type: "data",
   schema: z.record(
@@ -8,4 +26,4 @@ const locale = defineCollection({
   ),
 });
 
-export const collections = { locale };
+export const collections = { distro, locale };
